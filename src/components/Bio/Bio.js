651adf93f0c1d0a9e1e310d02fde4ead4b8d48a7
An Nguyen <an.nguyen@tuni.fi>
2024-12-24 20:39:00 +0200

    Up to date Bio information

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -3,7 +3,6 @@ import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timel
 import 'react-vertical-timeline-component/style.min.css';
 import { MdSchool, MdWork } from "react-icons/md";
 import {AiFillStar} from "react-icons/ai"
-import {GiNewBorn} from 'react-icons/gi'
 export const Bio = () => {
 
     return (
@@ -18,7 +17,34 @@ export const Bio = () => {
                         className="vertical-timeline-element--work"
                         contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                         contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                        date="10/2021 - present"
+                        date="10/2021 - Present"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        icon={<MdWork />}
+                    >
+                        <Heading as={"h3"} size="md" className="vertical-timeline-element-title">Junior Full-stack web developer</Heading>
+                        <Heading as={"h4"} size="sm" className="vertical-timeline-element-subtitle" whiteSpace="nowrap">Tampere, Finland</Heading>
+                        <Text>
+                            Full-stack developer in charge of web development for many projects using ReactJS and Ruby on Rails
+                        </Text>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        contentStyle = {{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                        date="9/2023 - 6/2025"
+                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                        icon={<MdSchool />}
+                    >
+                        <Heading as={"h3"} size="md" className="vertical-timeline-element-title">Tampere University, Science and Engineering</Heading>
+                        <Heading as={"h4"} size="sm" className="vertical-timeline-element-subtitle">Master Degree</Heading>
+                        <Text>
+                            Software, Web, and Cloud
+                        </Text>
+                    </VerticalTimelineElement>  
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                        date="10/2021 - 5/2023"
                         iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                         icon={<MdWork />}
                     >
@@ -31,6 +57,7 @@ export const Bio = () => {
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
                         date="1/2020 - 8/2020"
+                        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                         iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                         icon={<MdWork />}
                     >
@@ -43,7 +70,7 @@ export const Bio = () => {
                     <VerticalTimelineElement
                         className="vertical-timeline-element--education"
                         contentStyle = {{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                        date="9/2020 - Present"
+                        date="9/2020 - 6/2023"
                         iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
                         icon={<MdSchool />}
                     >
@@ -56,6 +83,7 @@ export const Bio = () => {
                     <VerticalTimelineElement
                         className="vertical-timeline-element--education"
                         date="5/2017 - 7/2020"
+                        contentStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
                         iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
                         icon={<MdSchool />}
                     >
@@ -64,15 +92,6 @@ export const Bio = () => {
                         <Text>
                             Physics major
                         </Text>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--birth"
-                        date="September 2002"
-                        contentStyle={{ background: 'rgb(255, 191, 0)', color: '#fff' }}
-                        iconStyle={{ background: 'rgb(255, 191, 0)', color: '#fff' }}
-                        icon={<GiNewBorn />} 
-                    >
-                        <Heading as={"h3"} size="md" className="vertical-timeline-element-title">Born in Ho Chi Minh city, Viet Nam</Heading>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
